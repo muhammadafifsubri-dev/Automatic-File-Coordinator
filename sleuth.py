523bb7f7f7e50a2475368951122d567f1de32ece
muhammadafifsubri-dev <muhammadafifsubri@gmail.com>
2026-04-25 00:57:51 +0800

    Add files via upload
    fixed : the current directory only for moving the file using glob instead of rglob

--- sleuth.py
+++ sleuth.py
@@ -1,8 +1,27 @@
+# sleuth(Automated File Coordinator)
+# still under development
+# Open-Source code developed by AfifSubri
+
 import os
 from pathlib import Path
 import shutil
 
+#source of the path
 src = '.'
-for path in Path(src).rglob('*.py'):
-    shutil.move(path, "python-stuff/")
-    break
+    
+#format txt    
+for path in Path('.').glob('*.txt'):
+    print(path)
+    shutil.move(path, "text-stuff/")
+    continue
+    
+#format pdf    
+for path in Path('.').glob('*.pdf'):
+    print(path)
+    shutil.move(path, "pdf-stuff/")
+    continue 
+    
+#format jpg
+for path in Path('.').glob('*.jpg'):
+    shutil.move(path, "jpg-stuff/")
+    continue    
